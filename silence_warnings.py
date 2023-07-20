31ed4dbd7a01ae9b42bce7c0213aa723968d4c32
Wojciech Migda <wojtek.golf@interia.pl>
2023-07-21 01:32:05 +0200

    Silence another UserWarning

--- silence_warnings.py
+++ silence_warnings.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import os
-os.environ['PYTHONWARNINGS']='ignore::DeprecationWarning,ignore:n_quantiles:UserWarning'
+os.environ['PYTHONWARNINGS']='ignore::DeprecationWarning,ignore:n_quantiles :UserWarning,ignore:Persisting input arguments took:UserWarning'
 
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning, message=".*Bins whose width are too small.*")
 warnings.filterwarnings("ignore", category=UserWarning, message=".*n_quantiles \([0-9]+\) is greater than the total number of samples.*")
+warnings.filterwarnings("ignore", category=UserWarning, message=".*Persisting input arguments took.*")
