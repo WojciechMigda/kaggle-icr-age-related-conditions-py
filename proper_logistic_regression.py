aed3fdc855c1b7c05cca10786cfda833095a2665
Wojciech Migda <wojtek.golf@interia.pl>
2023-07-28 00:16:38 +0200

    Add comment describing ProperLogisticRegression improvement

--- proper_logistic_regression.py
+++ proper_logistic_regression.py
@@ -5,6 +5,10 @@ from sklearn.linear_model import LogisticRegression
 
 
 class ProperLogisticRegression(LogisticRegression):
+    """ProperLogisticRegression improves over LogisticRegression
+    (which is a classifier) to return positive label probability
+    as result of predict().
+    """
     def predict(self, *args, **kwargs):
         return super().predict_proba(*args, **kwargs)[:, 1]
 
