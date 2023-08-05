becb4059ada67120597de2b15951605c2178cf84
Wojciech Migda <wojtek.golf@interia.pl>
2023-08-05 13:01:20 +0200

    Rename dict keys in trials_lr.py param sets

---
+++ trials_lr.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+lr_trial_53 = {
+    'lr__C': 0.00374377136399404,
+    'lr__max_iter': 5000,
+    'lr__class_weight': 29,
+    'random_state': 142,
+    'oe__fill_na': None,
+    'ii__max_iter': 50,
+    'ii__verbose': False,
+    'rfe__n_features_to_select': 495,
+    'rfe__verbose': False
+}
+
+# 2023-08-04 15:11 -> ICR-LogReg-basic / ICR-LogReg-oev2, cv=8
+lr_trial_97 = {
+    # cv=8 0.2984961090114193
+    "ii__max_iter": 50,
+    "lr__C": 0.0025521690067773586,
+    'lr__max_iter': 5000,
+    "lr__class_weight": 30,
+    "rfe__n_features_to_select": 430,
+    "random_state": 42,
+    'oe__fill_na': None,
+    'rfe__verbose': False,
+    'ii__verbose': False,
+}
+lr_trial_112 = {
+    # cv=8 0.2985386912366308
+    "ii__max_iter": 50,
+    "lr__C": 0.0026616460589121275,
+    'lr__max_iter': 5000,
+    "lr__class_weight": 61,
+    "rfe__n_features_to_select": 485,
+    "random_state": 42,
+    'oe__fill_na': None,
+    'rfe__verbose': False,
+    'ii__verbose': False,
+}
+lr_trial_99 = {
+    # cv=8 0.2986193480052779
+    "ii__max_iter": 50,
+    "lr__C": 0.002486116613972286,
+    'lr__max_iter': 5000,
+    "lr__class_weight": 44,
+    "rfe__n_features_to_select": 450,
+    "random_state": 42,
+    'oe__fill_na': None,
+    'rfe__verbose': False,
+    'ii__verbose': False,
+}
+lr_trial_102 = {
+    # cv=8 0.29926558530965913
+    "ii__max_iter": 50,
+    "lr__C": 0.0025932267285436548,
+    'lr__max_iter': 5000,
+    "lr__class_weight": 45,
+    "rfe__n_features_to_select": 440,
+    "random_state": 42,
+    'oe__fill_na': None,
+    'rfe__verbose': False,
+    'ii__verbose': False,
+}
+lr_trial_122 = {
+    # cv=8 0.29948356247346947
+    "ii__max_iter": 50,
+    "lr__C": 0.0038138680400989578,
+    'lr__max_iter': 5000,
+    "lr__class_weight": 64,
+    "rfe__n_features_to_select": 465,
+    "random_state": 42,
+    'oe__fill_na': None,
+    'rfe__verbose': False,
+    'ii__verbose': False,
+}
