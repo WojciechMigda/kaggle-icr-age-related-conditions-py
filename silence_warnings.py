1e2b4231ebfe5e239f96a5d5187579fd89786f45
Wojciech Migda <wojtek.golf@interia.pl>
2023-08-05 23:18:57 +0200

    Silence another warning

--- silence_warnings.py
+++ silence_warnings.py
@@ -5,6 +5,8 @@ import os
 os.environ['PYTHONWARNINGS']=",".join([
     'ignore::DeprecationWarning',
     'ignore:n_quantiles :UserWarning',
+    'ignore:Bins whose width are too small:UserWarning',
+    'ignore:X does not have valid feature names:UserWarning',
     'ignore:Persisting input arguments took:UserWarning',
     'ignore:Setting the eps parameter is deprecated and will be removed in 1.5. Instead eps will always havea default value of:FutureWarning',
     'ignore:suggest_loguniform has been deprecated in:FutureWarning',
@@ -14,6 +16,7 @@ os.environ['PYTHONWARNINGS']=",".join([
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning, message=".*Bins whose width are too small.*")
+warnings.filterwarnings("ignore", category=UserWarning, message=".*X does not have valid feature names.*")
 warnings.filterwarnings("ignore", category=UserWarning, message=".*n_quantiles \([0-9]+\) is greater than the total number of samples.*")
 warnings.filterwarnings("ignore", category=UserWarning, message=".*Persisting input arguments took.*")
 warnings.filterwarnings("ignore", category=FutureWarning, message=".*Setting the eps parameter is deprecated and will be removed in 1.5. Instead eps will always havea default value of.*")
