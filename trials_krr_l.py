1c3984cc367e07f0515ebd86f0b2330a55a693e6
Wojciech Migda <wojtek.golf@interia.pl>
2023-08-07 23:40:02 +0200

    Add KRR Laplacian trials file

---
+++ trials_krr_l.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# 2023-08-04 15:21 -> ICR-KernelRidge-laplacian / ICR-LogReg-oev2
+krr_lap_trial_335 = {
+    # cv=8 0.35450684683678696
+    "kernel": "laplacian",
+    "laplacian:alpha": 1.97394658182408,
+    "laplacian:class_weight": 37,
+    "laplacian:gamma": 0.003922926685631318,
+    "random_state": 42,
+    'oe__fill_na': None,
+    'ii__max_iter': 70,
+    'ii__verbose': False,
+    'rfe__n_features_to_select': 325,
+    'rfe__verbose': False,
+    'lr__class_weight': 30,
+    'lr__max_iter': 5000,
+    'lr__C': 0.002,
+}
+krr_lap_trial_362 = {
+    # cv=8 0.35451416450816026
+    "kernel": "laplacian",
+    "laplacian:alpha": 1.9593124658305436,
+    "laplacian:class_weight": 40,
+    "laplacian:gamma": 0.003112608274555699,
+    "random_state": 42,
+    'oe__fill_na': None,
+    'ii__max_iter': 50,
+    'ii__verbose': False,
+    'rfe__n_features_to_select': 400,
+    'rfe__verbose': False,
+    'lr__class_weight': 30,
+    'lr__max_iter': 5000,
+    'lr__C': 0.002,
+}
+krr_lap_trial_363 = {
+    # cv=8 0.35460671657431503
+    "kernel": "laplacian",
+    "laplacian:alpha": 2.162989409404174,
+    "laplacian:class_weight": 39,
+    "laplacian:gamma": 0.006343544664861158,
+    "random_state": 42,
+    'oe__fill_na': None,
+    "ii__max_iter": 50,
+    'ii__verbose': False,
+    "rfe__n_features_to_select": 405,
+    'rfe__verbose': False,
+    'lr__class_weight': 30,
+    'lr__max_iter': 5000,
+    'lr__C': 0.002,
+}
+krr_lap_trial_327 = {
+    # cv=8 0.35489084653891256
+    "kernel": "laplacian",
+    "II max_iter": 70,
+    "KRR alpha laplacian": 2.223713602825327,
+    "KRR class_weight": 33,
+    "KRR laplacian gamma": 0.002824346010384687,
+    "RFE n_features_to_select": 325,
+    "random_state": 42,
+}
+krr_lap_trial_313 = {
+    # cv=8 0.35497101935085534
+    "kernel": "laplacian",
+    "II max_iter": 70,
+    "KRR alpha laplacian": 1.7408936778973814,
+    "KRR class_weight": 37,
+    "KRR laplacian gamma": 0.0050546139257194155,
+    "RFE n_features_to_select": 415,
+    "random_state": 42,
+}
